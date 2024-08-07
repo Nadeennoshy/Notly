8e7a20bdb861ce4b6e4a250aa8cfc978770bb524
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-08-07 23:45:44 +0300

    create onBoarding screen

---
+++ lib/widgets/onboarding_body.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingBody extends StatelessWidget {
+  const OnBoardingBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.centerRight,
+          children: [
+            Image.asset('assets/images/Wall Polygon.png'),
+            Positioned(
+              top: 80,
+              right: 80,
+              child: Image.asset(
+                'assets/images/onBoarding.png',
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Center(
+          child: Text(
+            'Create free notes & collaborate',
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+          ),
+        ),
+        const Text(
+          'with your team',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
+        const SizedBox(height: 15,),
+        const Center(
+          child: Text(
+            'Start organizing your life and unleashing ',
+          ),
+        ),
+        const Text('your creativity with Notly App'),
+        const SizedBox(height: 40,),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 100),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              backgroundColor: const Color(0xffFF8383),
+              minimumSize: const Size(90, 50),
+                
+            ),
+           onPressed: (){},
+           child: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Get Started',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              ),
+              Icon(Icons.arrow_right_alt_sharp,color: Colors.white,)
+            ],
+           )),
+        )
+      ],
+    );
+  }
+}
