e41f28e3e81407a1dbf1dd92ffd6e95e01436e5c
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-08-09 04:49:08 +0300

    provide add note cubit

--- lib/cubits/add_note/add_note_cubit.dart
+++ lib/cubits/add_note/add_note_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:meta/meta.dart';
 import 'package:notly/constants.dart';
 import 'package:notly/models/note_model.dart';
 
--- lib/widgets/add_note_body.dart
+++ lib/widgets/add_note_body.dart
@@ -1,36 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:notly/cubits/add_note/add_note_cubit.dart';
 import 'package:notly/widgets/colors_list_view.dart';
 import 'package:notly/widgets/custom_button.dart';
 import 'package:notly/widgets/custom_text_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddNoteBody extends StatelessWidget {
   const AddNoteBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-         padding: const EdgeInsets.all(16.0),
-         child: ListView(
-          children: const [
-            SizedBox(height: 10,),
-            Text('Add Note',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color(0xffFF8383),
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),),
-            SizedBox(height: 30,),
-            CustomTextField(hintText: 'Enter Title'),
-            SizedBox(height: 15,),
-            CustomTextField(hintText: 'Details',maxLines: 6,),
-            SizedBox(height: 20,),
-            ColorsListView(),
-            SizedBox(height: 30,),
-            CustomButton(buttonName: 'Add',),
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if(state is AddNoteSuccess){
+              showDialog(context: context, builder: (context){
+                return const AlertDialog();
+              });
+            }else if(state is AddNoteFailure){
 
-          ],
-         ),
-       );
+            }
+          },
+          builder: (context, state) {
+            return ListView(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Add Note',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xffFF8383),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const CustomTextField(hintText: 'Enter Title'),
+                const SizedBox(
+                  height: 15,
+                ),
+                const CustomTextField(
+                  hintText: 'Details',
+                  maxLines: 6,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const ColorsListView(),
+                const SizedBox(
+                  height: 30,
+                ),
+                state is AddNoteLoading? const CircularProgressIndicator(): const CustomButton(
+                  buttonName: 'Add',
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
