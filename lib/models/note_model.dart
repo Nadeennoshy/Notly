a8e5716652f1ae0145cb11728ac95355d5575739
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-08-09 03:44:56 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel{
+  String title;
+  String subTitle;
+  String date;
+  int color;
+
+  NoteModel({required this.color,required this.date,required this.subTitle,required this.title});
+}
